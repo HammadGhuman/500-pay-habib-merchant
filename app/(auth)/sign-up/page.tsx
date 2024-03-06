17ca130c33022bbe63c5aba4e1763a184d764938
Hammad Ghuman <ghumanhammad420@gmail.com>
2024-03-06 21:10:24 +0500

    first commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Sign UP</div>;
+}
+
+export default page;
---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "flowbite.s3.amazonaws.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
